17fe48b142d4544f94ee12fc8e6c25c8ab5a0c88
richasingh-16 <s.richaaaaxyz@gmail.com>
2026-04-08 16:26:42 +0530

    fix prisma client path and build script

--- hospital-backend/src/config/prisma.ts
+++ hospital-backend/src/config/prisma.ts
@@ -3,7 +3,7 @@ dotenv.config()
 
 import { Pool } from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../generated/prisma"
 
 const connectionString = `${process.env.DATABASE_URL}`
 const pool = new Pool({ connectionString })
