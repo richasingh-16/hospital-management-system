afa5e1450f049477ced25c86a33a99a400dec637
richasingh-16 <s.richaaaaxyz@gmail.com>
2026-04-08 16:21:02 +0530

    preivew in prisma

--- hospital-backend/src/config/prisma.ts
+++ hospital-backend/src/config/prisma.ts
@@ -3,12 +3,11 @@ dotenv.config()
 
 import { Pool } from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "../generated/prisma"
-
+import { PrismaClient } from "@prisma/client"
 
 const connectionString = `${process.env.DATABASE_URL}`
 const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg(pool, { schema: "public" })
 const prisma = new PrismaClient({ adapter })
 
 export default prisma
